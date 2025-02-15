638949200725a7dab08dde5ef4a1c01264f3e97c
Igor <golovkovviggreserv@gmail.com>
2025-02-16 01:25:20 +0300

    .....

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
+import { EventEmitter } from './components/base/events';
 import './scss/styles.scss';
+import { API_URL } from './utils/constants';
+
+fetch('https://larek-api.nomoreparties.co/weblarek.postman.json')
+	.then((res) => res.json())
+	.then((res) => {
+		console.log(res.item);
+	});
+
+class CardItem {
+	protected openButton: HTMLButtonElement;
+	constructor(
+	) {
+		this.openButton = document.querySelector('.header__basket');
+		this.openButton.addEventListener('click', () => {
+			console.log('Работает');
+		});
+	}
+}
+
+new CardItem()
