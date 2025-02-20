7f3d3109a918dc1637744536ce46d2785b4667e6
Igor <golovkovviggreserv@gmail.com>
2025-02-20 17:01:14 +0300

    вывод карточек на экран

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,48 @@
 import { Api } from './components/base/api';
 import { EventEmitter } from './components/base/events';
 import './scss/styles.scss';
-import { CatalogItem } from './types';
+import { CatalogItemCard } from './types';
 import { API_URL, CDN_URL } from './utils/constants';
+import { cloneTemplate, ensureElement } from './utils/utils';
 
+const cardCatalogTemplate = ensureElement<HTMLTemplateElement>('#card-catalog');
+const gallery = document.querySelector('.gallery')
 fetch(`https://larek-api.nomoreparties.co/api/weblarek/product`)
 	.then((res) => res.json())
 	.then((res) => {
-		console.log(res);
+		renderCatalog(res.items as []);
+	})
+	.catch((error) => {
+		console.log(error);
 	});
 
 class CardItem {
-	protected openButton: HTMLButtonElement;
-	constructor() {
-		this.openButton = document.querySelector('.header__basket');
-		this.openButton.addEventListener('click', () => {
-			console.log('Работает');
-		});
+	public cards: CatalogItemCard;
+	protected category: HTMLElement;
+	protected title: HTMLElement;
+	protected image: HTMLImageElement;
+	protected price: HTMLElement;
+
+	getItem(card: CatalogItemCard, element:HTMLElement){
+		
+		this.category = element.querySelector('.card__category_soft');
+		this.title = element.querySelector('.card__title');
+		this.image = element.querySelector('.card__image');
+		this.price = element.querySelector('.card__price');
+
+		this.category.textContent = card.category
+		this.title.textContent = card.title
+		this.image.src = card.image
+		
+		return element
 	}
 }
-function renderCatalog() {}
 
-new CardItem();
+function renderCatalog(card: []) {
+	const carditem = new CardItem()
+	card.forEach((element) => {
+		gallery.append(carditem.getItem(element, cloneTemplate(cardCatalogTemplate)))
+	});
+}
+
+
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
+
 export interface CatalogItemCard {
 	category: string;
 	description: string;
