95db24421435c16a01e32c0872b3f2c560a3c141
Igor <golovkovviggreserv@gmail.com>
2025-03-08 01:22:45 +0300

    добавление в корзину todo

--- src/components/Form.ts
+++ src/components/Form.ts
@@ -1,5 +1,8 @@
 export class Form {
-  protected input: HTMLInputElement
+  protected input: HTMLInputElement;
+  protected email: string;
+  protected phone: string;
+  protected adress: string;
   constructor(input: HTMLInputElement){
     this.input = input
   }
--- src/components/basket.ts
+++ src/components/basket.ts
@@ -12,9 +12,13 @@ export class BasketModel implements IBasketModel {
 
   }
   public add(id: string) {
+    if(this.items.has(id)) this.items.set(id, 0)
+    this.items.set(id, this.items.get(id) + 1)
 		this._changed();
 	}
 	public remove(id: string) {
+    //доделать
+    if(this.items)
 		this._changed();
 	}
 	protected _changed() {
