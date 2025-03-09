a75f837d0b9b7ea6ed69c7ba1cc2a8eeb501e61e
Igor <golovkovviggreserv@gmail.com>
2025-03-09 23:56:06 +0300

    Form

--- src/components/Form.ts
+++ src/components/Form.ts
@@ -3,7 +3,19 @@ export class Form {
   protected email: string;
   protected phone: string;
   protected adress: string;
-  constructor(input: HTMLInputElement){
-    this.input = input
+  constructor(container: HTMLInputElement){
+    this.input = container
+  }
+
+  set setEmail(email: string) {
+    this.email = email
+  }
+
+  set setPhone(phone: string) {
+    this.phone = phone
+  }
+
+  set setAdress(adress: string) {
+    this.adress = adress
   }
 }
